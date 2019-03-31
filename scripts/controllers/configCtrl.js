04621e89d91ffb46baceadfe8fd3b5bc8dea198c
Terazus <batistadominique@hotmail.com>
2019-03-31 06:27:16 -0700

    added  request

--- scripts/controllers/configCtrl.js
+++ scripts/controllers/configCtrl.js
@@ -10,7 +10,41 @@ angular.module('appConfigCtrl', []).controller(
             $scope.data = {
                 description: "This_is_a_description_with_removed_underscores",
                 field: "This is a field"
-            }
+            };
+
+            $scope.request = {
+                method: 'GET',
+                url: null,
+                headers: {
+                    'Accept': "application/json",
+                },
+                data: null,
+                //timeout: 2000
+            };
+
+            $scope.executeCall = function(){
+                try{
+                    $http($scope.request).then(function(response){
+                        $scope.data = response.data;
+                    }, function(error){
+                        $scope.data = {
+                            message: "There is an error with your request, see below",
+                            error: error,
+                            request: $scope.request
+                        };
+                    })
+                }
+                catch (e){
+                    $scope.data = {
+                        message: "There is an error with your request, see below",
+                        error: e,
+                        request: $scope.request
+                    };
+                }
+
+            };
+
+            $scope.executeCall();
         }
     ]
 );
